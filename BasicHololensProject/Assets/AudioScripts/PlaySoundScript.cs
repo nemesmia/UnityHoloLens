1fdecd205d7e7e125ef88c7d2113535b88d16e8d
Nemes <NemesM@haufe.com>
2017-11-06 22:40:53 +0100

    Play a sound when clicking on a button

---
+++ BasicHololensProject/Assets/AudioScripts/PlaySoundScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlaySoundScript : MonoBehaviour {
+    public AudioClip MyAudioFile;
+
+    public void PlaySound()
+    {
+        GetComponent<AudioSource>().PlayOneShot(MyAudioFile);
+    }
+}
